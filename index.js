ac7af3c3138adb4eda28d17d454b748d115bad7f
nilton <nfescame@gmail.comq:qclear>
2022-04-09 00:04:59 -0300

    final

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ require("dotenv").config();
 require("./config/db.config")();
 
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 app.get("/", (req, res) => {
   res.status(200).json(DB);
