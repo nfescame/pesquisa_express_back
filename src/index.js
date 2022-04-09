0f96892c5ae1f6e28ba3d64712f09af3c642d692
nilton <nfescame@gmail.comq:qclear>
2022-04-09 18:23:01 -0300

    remove morgan

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const app = express();
 require("dotenv").config();
 require("../config/db.config")();
 
-app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
