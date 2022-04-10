c033ffb5827b4cb16c0a8c9f66dcb26c136f4c28
nilton <nfescame@gmail.comq:qclear>
2022-04-10 20:36:49 -0300

    newRoutes

--- src/index.js
+++ src/index.js
@@ -20,9 +20,53 @@ app.get("/", (req, res) => {
 });
 
 app.get("/vote", async (req, res) => {
-  const vote = await VoteModel.find();
+  const votes = await VoteModel.find();
 
-  res.status(200).json(vote);
+  res.status(200).json(votes);
+});
+
+app.get("/ipv4", async (req, res) => {
+  const votes = await VoteModel.find();
+
+  const ipv4 = votes.map((vote) => {
+    let i = vote.ip[0].IPv4;
+    let candidate = vote.candidate;
+    return { i, candidate };
+  });
+  res.status(200).json(ipv4);
+});
+
+app.get("/country", async (req, res) => {
+  const votes = await VoteModel.find();
+
+  const country = votes.map((vote) => {
+    let country_name = vote.ip[0].country_name;
+    let candidate = vote.candidate;
+    return { country_name, candidate };
+  });
+  res.status(200).json(country);
+});
+
+app.get("/city", async (req, res) => {
+  const votes = await VoteModel.find();
+
+  const city = votes.map((vote) => {
+    let city = vote.ip[0].city;
+    let candidate = vote.candidate;
+    return { city, candidate };
+  });
+  res.status(200).json(city);
+});
+
+app.get("/state", async (req, res) => {
+  const votes = await VoteModel.find();
+
+  const state = votes.map((vote) => {
+    let state_name = vote.ip[0].state;
+    let candidate = vote.candidate;
+    return { state_name, candidate };
+  });
+  res.status(200).json(state);
 });
 
 app.post("/vote", async (req, res, next) => {
