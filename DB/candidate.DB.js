13a8d109c3d960786613a92bc76971ad8ecefbf2
nilton <nfescame@gmail.comq:qclear>
2022-04-23 22:17:52 -0300

    incluido ciro

--- DB/candidate.DB.js
+++ DB/candidate.DB.js
@@ -5,6 +5,7 @@ const data = {
     { id: 3, name: "João Dória" },
     { id: 4, name: "Sérgio Moro" },
     { id: 5, name: "Eduardo Leite" },
+    { id: 7, name: "Ciro Gomes" },
     { id: 6, name: "Nulo" },
   ],
 };
